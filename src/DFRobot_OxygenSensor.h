1127569fa0f27f773139bb996f4342e14e74a5ab
jiaziui <3273174295@qq.com>
2025-09-30 17:45:15 +0800

    ok

--- src/DFRobot_OxygenSensor.h
+++ src/DFRobot_OxygenSensor.h
@@ -48,7 +48,7 @@ typedef enum { eVersionError = -1, eProbenExhausted = 0, eProbenNormal = 1 } ePr
 class DFRobot_OxygenSensor {
 public:
   DFRobot_OxygenSensor(TwoWire *pWire = &Wire);
-  ~DFRobot_OxygenSe nsor();
+  ~DFRobot_OxygenSensor();
   /**
    * @fn begin
    * @brief Initialize i2c
